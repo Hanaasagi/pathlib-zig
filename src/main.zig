a190616afe89c96a2220e4bb20a11167fbb436ab
Hanaasagi <ambiguous404@gmail.com>
2023-05-12 20:32:40 +0900

    add some methods

--- src/main.zig
+++ src/main.zig
@@ -7,6 +7,27 @@ const open_flags = .{
     .access_sub_paths = true,
 };
 
+fn matchPattern(pattern: []const u8, str: []const u8) bool {
+    if (mem.eql(u8, pattern, "*")) return true;
+
+    var i: usize = 0;
+    var it = mem.tokenize(u8, pattern, "*");
+    var exact_begin = pattern.len > 0 and pattern[0] != '*';
+
+    while (it.next()) |substr| {
+        if (mem.indexOf(u8, str[i..], substr)) |j| {
+            if (exact_begin) {
+                if (j != 0) return false;
+                exact_begin = false;
+            }
+
+            i += j + substr.len;
+        } else return false;
+    }
+
+    return if (pattern[pattern.len - 1] == '*') true else i == str.len;
+}
+
 pub const Iterator = struct {
     allocator: mem.Allocator,
     root: std.fs.IterableDir,
@@ -54,27 +75,6 @@ pub const Iterator = struct {
         if (self.path) |path| self.allocator.free(path);
     }
 
-    pub fn match(pattern: []const u8, str: []const u8) bool {
-        if (mem.eql(u8, pattern, "*")) return true;
-
-        var i: usize = 0;
-        var it = mem.tokenize(u8, pattern, "*");
-        var exact_begin = pattern.len > 0 and pattern[0] != '*';
-
-        while (it.next()) |substr| {
-            if (mem.indexOf(u8, str[i..], substr)) |j| {
-                if (exact_begin) {
-                    if (j != 0) return false;
-                    exact_begin = false;
-                }
-
-                i += j + substr.len;
-            } else return false;
-        }
-
-        return if (pattern[pattern.len - 1] == '*') true else i == str.len;
-    }
-
     pub fn next(self: *Iterator) !?[]const u8 {
         if (self.done) return null;
 
@@ -89,7 +89,7 @@ pub const Iterator = struct {
                 if (entry.kind != .File and entry.kind != .Directory)
                     continue;
 
-                if (match(self.segments.items[i], entry.name)) switch (entry.kind) {
+                if (matchPattern(self.segments.items[i], entry.name)) switch (entry.kind) {
                     .File => {
                         if (self.path) |path| {
                             self.allocator.free(path);
@@ -162,6 +162,10 @@ pub const PathLib = struct {
     pub fn home(self: PathLib) Path {
         return Path.init(self.allocator, get_home_dir(), false);
     }
+
+    pub fn cwd(self: PathLib) !Path {
+        return Path.init(self.allocator, try std.process.getCwdAlloc(self.allocator), true);
+    }
 };
 
 pub const Path = struct {
@@ -227,17 +231,50 @@ pub const Path = struct {
         return !std.fs.path.isAbsolute(self.toSlice());
     }
 
+    pub fn absolute(self: Path) !Path {
+        if (self.isAbsolute()) {
+            return self;
+        }
+        const cwd = try PathLib.init(self.allocator).cwd();
+        defer cwd.deinit();
+        return cwd.joinPath(&[_]Path{self});
+    }
+
     pub fn name(self: Path) []const u8 {
         return std.fs.path.basename(self.toSlice());
     }
 
+    pub fn withName(self: Path, newName: []const u8) !Path {
+        const base = std.fs.path.dirname(self.toSlice()) orelse "/";
+        const sep = std.fs.path.sep_str;
+
+        var newPath = try self.allocator.alloc(u8, base.len + newName.len + sep.len);
+        std.mem.copy(u8, newPath[0..], base);
+        std.mem.copy(u8, newPath[base.len..], sep);
+        std.mem.copy(u8, newPath[base.len + sep.len ..], newName);
+
+        return Path.init(self.allocator, newPath, true);
+    }
+
     pub fn parent(self: Path) Path {
         const p = std.fs.path.dirname(self.toSlice()) orelse "/";
-        return Path.init(self.allocator, p, self.fromHeap);
+
+        return Path.init(self.allocator, p, false);
     }
 
     pub fn suffix(self: Path) []const u8 {
         return std.fs.path.extension(self.toSlice());
+    }
+
+    pub fn withSuffix(self: Path, newSuffix: []const u8) !Path {
+        const path = self.toSlice();
+        const ext = std.fs.path.extension(path);
+
+        var newPath = try self.allocator.alloc(u8, path.len - ext.len + newSuffix.len);
+        std.mem.copy(u8, newPath[0..], path[0 .. path.len - ext.len]);
+        std.mem.copy(u8, newPath[path.len - ext.len ..], newSuffix);
+
+        return Path.init(self.allocator, newPath, true);
     }
 
     pub fn suffixes(self: Path) !std.ArrayList([]const u8) {
@@ -260,9 +297,16 @@ pub const Path = struct {
         var list = std.ArrayList([]const u8).init(self.allocator);
         defer list.deinit();
 
+        const sep = std.fs.path.sep_str;
+
         try list.append(self.toSlice());
         for (others) |p| {
-            try list.append(p.toSlice());
+            const s = p.toSlice();
+            if (std.mem.startsWith(u8, s, "." ++ sep)) {
+                try list.append(s[1..]);
+            } else {
+                try list.append(s);
+            }
         }
 
         const newPath = try std.fs.path.join(self.allocator, list.items);
@@ -347,10 +391,56 @@ pub const Path = struct {
         return !try self.isDir();
     }
 
+    pub fn match(self: Path, pattern: []const u8) bool {
+        return matchPattern(pattern, self.toSlice());
+    }
+
     pub fn glob(self: Path, pattern: []const u8) !Iterator {
         var dir = try std.fs.cwd().openIterableDir(self.toSlice(), open_flags);
 
         return try Iterator.init(self.allocator, dir, pattern);
+    }
+
+    pub fn exists(self: Path) !bool {
+        _ = self.stat() catch {
+            return false;
+        };
+
+        return true;
+    }
+
+    pub fn openFile(self: Path, flags: std.fs.File.OpenFlags) std.fs.File.OpenError!std.fs.File {
+        const path = self.toSlice();
+        if (self.isAbsolute()) {
+            if (!try self.exists()) {
+                const f = try std.fs.createFileAbsolute(path, .{ .read = true, .truncate = false });
+                defer f.close();
+            }
+            return std.fs.openFileAbsolute(path, flags);
+        }
+
+        if (!try self.exists()) {
+            const f = try std.fs.cwd().createFile(path, .{ .read = true, .truncate = false });
+            defer f.close();
+        }
+
+        return std.fs.cwd().openFile(path, flags);
+    }
+
+    pub fn openDir(self: Path, flags: std.fs.Dir.OpenDirOptions) !std.fs.Dir {
+        const path = self.toSlice();
+        if (self.isAbsolute()) {
+            if (!try self.exists()) {
+                try std.fs.makeDirAbsolute(path);
+            }
+            return std.fs.openDirAbsolute(self.toSlice(), flags);
+        }
+
+        if (!try self.exists()) {
+            try std.fs.cwd().makeDir(path);
+        }
+
+        return std.fs.cwd().openDir(self.toSlice(), flags);
     }
 };
 
@@ -366,6 +456,16 @@ test "test home" {
     defer path.deinit();
 
     const expect: []const u8 = std.os.getenv("HOME") orelse "";
+
+    try std.testing.expect(std.mem.eql(u8, path.toSlice(), expect));
+}
+
+test "test cwd" {
+    const path = try PathLib.init(std.testing.allocator).cwd();
+    defer path.deinit();
+
+    const expect: []const u8 = try std.process.getCwdAlloc(std.testing.allocator);
+    defer std.testing.allocator.free(expect);
 
     try std.testing.expect(std.mem.eql(u8, path.toSlice(), expect));
 }
@@ -398,6 +498,25 @@ test "test isAbsolute" {
     try std.testing.expect(lib.fromSlice("./zig/src").isAbsolute() == false);
 
     try std.testing.expect(lib.fromSlice("/home/zig/src").isAbsolute() == true);
+}
+
+test "test absolute" {
+    const path = PathLib.init(std.testing.allocator).fromSlice("./zig/src");
+    defer path.deinit();
+
+    const absPath = try path.absolute();
+    defer absPath.deinit();
+
+    const cwd: []const u8 = try std.process.getCwdAlloc(std.testing.allocator);
+    defer std.testing.allocator.free(cwd);
+
+    var expect = try std.testing.allocator.alloc(u8, cwd.len + "/zig/src".len);
+    std.mem.copy(u8, expect[0..], cwd);
+    std.mem.copy(u8, expect[cwd.len..], "/zig/src");
+
+    defer std.testing.allocator.free(expect);
+
+    try std.testing.expect(std.mem.eql(u8, absPath.toSlice(), expect));
 }
 
 test "test isRelative" {
@@ -438,6 +557,18 @@ test "test suffix" {
     try std.testing.expect(std.mem.eql(u8, path.suffix(), ""));
 }
 
+test "test withSuffix" {
+    const lib = PathLib.init(std.testing.allocator);
+
+    var path = lib.fromSlice("my/library/setup.py");
+    defer path.deinit();
+
+    var newPath = try path.withSuffix(".zig");
+    defer newPath.deinit();
+
+    try std.testing.expect(std.mem.eql(u8, newPath.toSlice(), "my/library/setup.zig"));
+}
+
 test "test suffixes" {
     const lib = PathLib.init(std.testing.allocator);
 
@@ -475,6 +606,16 @@ test "test name" {
     defer path.deinit();
 
     try std.testing.expect(std.mem.eql(u8, path.name(), "setup.py"));
+}
+
+test "test withName" {
+    const path = PathLib.init(std.testing.allocator).fromSlice("my/library/setup.py");
+    defer path.deinit();
+
+    const newPath = try path.withName("install.py");
+    defer newPath.deinit();
+
+    try std.testing.expect(std.mem.eql(u8, newPath.toSlice(), "my/library/install.py"));
 }
 
 test "test stat file" {
@@ -545,6 +686,36 @@ test "test isFile" {
     try std.testing.expect(res == true);
 }
 
+test "test match" {
+    var path = PathLib.init(std.testing.allocator).fromSlice("b.zig");
+    defer path.deinit();
+    try std.testing.expect(path.match("*.zig") == true);
+
+    path = PathLib.init(std.testing.allocator).fromSlice("/b.zig");
+    defer path.deinit();
+    try std.testing.expect(path.match("*.zig") == true);
+}
+
+test "test match nested" {
+    var path = PathLib.init(std.testing.allocator).fromSlice("a/b.zig");
+    defer path.deinit();
+    try std.testing.expect(path.match("*.zig") == true);
+
+    path = PathLib.init(std.testing.allocator).fromSlice("a/b/c.zig");
+    defer path.deinit();
+
+    try std.testing.expect(path.match("a/b/c.zig") == true);
+    try std.testing.expect(path.match("b/c.zig") == false);
+    try std.testing.expect(path.match("c.zig") == false);
+
+    try std.testing.expect(path.match("*/b/c.zig") == true);
+    try std.testing.expect(path.match("*/b/*.zig") == true);
+    try std.testing.expect(path.match("*/*/*.zig") == true);
+    try std.testing.expect(path.match("*.zig") == true);
+    // TODO: ?
+    try std.testing.expect(path.match("b/*.zig") == false);
+}
+
 test "test glob" {
     var path = PathLib.init(std.testing.allocator).fromSlice(".");
     defer path.deinit();
@@ -583,3 +754,49 @@ test "test isSocket" {}
 test "test isSymlink" {}
 
 test "test isFifo" {}
+
+test "test exists" {
+    var path = PathLib.init(std.testing.allocator).fromSlice("/dev/random");
+    defer path.deinit();
+    try std.testing.expect(try path.exists() == true);
+
+    path = PathLib.init(std.testing.allocator).fromSlice("/dev/random222");
+    defer path.deinit();
+    try std.testing.expect(try path.exists() == false);
+
+    path = PathLib.init(std.testing.allocator).fromSlice("./build.zig");
+    defer path.deinit();
+    try std.testing.expect(try path.exists() == true);
+}
+
+test "test openFile" {
+    var dst = std.testing.tmpDir(open_flags);
+    defer dst.cleanup();
+
+    var path = PathLib.init(std.testing.allocator).fromSlice("./zig-cache/tmp/" ++ dst.sub_path ++ "/test");
+    defer path.deinit();
+
+    try std.testing.expect(try path.exists() == false);
+
+    var file = try path.openFile(.{ .mode = std.fs.File.OpenMode.write_only });
+    defer file.close();
+
+    try std.testing.expect(try path.exists() == true);
+    try std.testing.expect(try path.isFile() == true);
+}
+
+test "test openDir" {
+    var dst = std.testing.tmpDir(open_flags);
+    defer dst.cleanup();
+
+    var path = PathLib.init(std.testing.allocator).fromSlice("./zig-cache/tmp/" ++ dst.sub_path ++ "/test");
+    defer path.deinit();
+
+    try std.testing.expect(try path.exists() == false);
+
+    var dir = try path.openDir(.{});
+    defer dir.close();
+
+    try std.testing.expect(try path.exists() == true);
+    try std.testing.expect(try path.isDir() == true);
+}
